c267e8e8ebe315f7a7a4865115475d1757736a73
m-gibas <gibasmaksymilian@gmail.com>
2022-07-21 21:39:59 +0200

    init

---
+++ JavaScript/collapsible.js
@@ -0,0 +1,16 @@
+let collapsible = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < collapsible.length; i++) {
+    collapsible[i].addEventListener("click", function() { 
+        this.classList.toggle("active");
+
+        let content = this.nextElementSibling;
+        if(content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } 
+        else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}

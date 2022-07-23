8a12948902b35aae63bd9d8f7e54dd37677e2b5a
m-gibas <gibasmaksymilian@gmail.com>
2022-07-24 00:51:30 +0200

    init

--- JavaScript/catalog.js
+++ JavaScript/catalog.js
@@ -1,7 +1,23 @@
-function openNav() {
-    document.getElementById("myProduct").style.width = "100%";
+function openNav1() {
+    document.getElementById("myProduct1").style.width = "100%";
+    setTimeout(function() {document.getElementById("overlay-text1").style.color = "black"}, 280);
   }
   
-  function closeNav() {
-    document.getElementById("myProduct").style.width = "0%";
+  function closeNav1() {
+    document.getElementById("myProduct1").style.width = "0%";
+    setTimeout(function() {document.getElementById("overlay-text1").style.color = "var(--clr-background"}, 50);
   }
+
+
+  function openNav2() {
+    document.getElementById("myProduct2").style.width = "100%";
+    setTimeout(function() {document.getElementById("overlay-text2").style.color = "black"}, 280);
+
+  }
+  
+  function closeNav2() {
+    document.getElementById("myProduct2").style.width = "0%";
+    setTimeout(function() {document.getElementById("overlay-text2").style.color = "var(--clr-background"}, 50);
+  }
+
+  

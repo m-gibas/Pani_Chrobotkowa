d5fc0c3cd57138ba5371444bf46786a59be3b5cc
m-gibas <gibasmaksymilian@gmail.com>
2022-07-24 00:01:46 +0200

    init

---
+++ JavaScript/catalog.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("myProduct").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myProduct").style.width = "0%";
+  }
